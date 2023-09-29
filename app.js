e59155015a356551be3e6dcb0253e5ebb2670ef4
Hiram Fernandes <hiram.sd@gmail.com>
2023-09-29 13:22:49 -0300

    Added db get methods

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 
-const mongoService = require('./database/mongo');
-
 dotenv.config();
+const mongoService = require('./database/mongo');
 
 const port = process.env.PORT_NUMBER || 3999;
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/', mongoService.savePurchase);
+app.post('/', mongoService.savePurchase);
+app.get('/', mongoService.getPurchases);
 
 app.listen(port);
--- database/mongo.js
+++ database/mongo.js
@@ -1,8 +1,9 @@
 const MongoClient = require('mongodb').MongoClient;
 
+const url = process.env.MONGODB_CONNECTION_URL || 'not found';
+const client = new MongoClient(url);
+
 const savePurchase = async (req, res, next) => {
-    const url = process.env.MONGODB_CONNECTION_URL || 'not found';
-    const client = new MongoClient(url);
 
     const newPurchase = {
         purchaseDate: '2023-09-23',
@@ -18,12 +19,23 @@ const savePurchase = async (req, res, next) => {
         res.json({message: `Could not insert product: ${error}`});
         return;
     }
-    // client.close();
 
     res.json(newPurchase);
 }
 
-const getPurchases = async (req, res, next) => {};
+const getPurchases = async (req, res, next) => {
+    let purchases;
+    try {
+        await client.connect();
+        const db = client.db('control');
+        purchases = await db.collection('purchases').find().toArray();
+    } catch (error) {
+        res.json({message: `Error while trying to retrieve purchases: ${error}`});
+        return;
+    }
+
+    res.json(purchases);
+};
 
 exports.savePurchase = savePurchase;
 exports.getPurchases = getPurchases;
