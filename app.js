31755dc502a0f15d96675cf59bd3561417212023
Hiram Fernandes <hiran.sd@gmail.com>
2023-10-10 21:19:24 -0300

    Added CORS setup to allow all requests

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// TODO: Review CORS setup to limit requests
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+    next();
+});
+
 // app.post('/', mongoService.savePurchase);
 // app.get('/', mongoService.getPurchases);
 
